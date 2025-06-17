79f79bcbbab1f541a67b6ff20f8f802c8721f121
Nununadia <nununadia27@gmail.com>
2025-06-17 22:07:37 +0700

    Update .gitignore and fix Cypress workflow

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,6 +1,17 @@
-describe('Verifikasi Fitur Login Akun', () => {
+describe('TS01 - cek fitur login', () => {
 
-  it('User Berhasil Login dengan data akun yang terdaftar', () => {
-      cy.Login('standard_user', 'secret_sauce')
+  it('Berhasil login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test="firstName"]').type('suep')
+    cy.get('[data-test="lastName"]').type('wayan')
+    cy.get('[data-test="postalCode"]').type('1234')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="finish"]').click()
   })
 })
---
+++ cypress/e2e/Loginpage.cy.js
@@ -0,0 +1,18 @@
+describe('TS01 - cek fitur login', () => {
+
+  it('Berhasil login', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
+    cy.get('[data-test="firstName"]').type('isyana')
+    cy.get('[data-test="lastName"]').type('mirip aku')
+    cy.get('[data-test="postalCode"]').type('1234')
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="finish"]').click()
+  })
+
+})
